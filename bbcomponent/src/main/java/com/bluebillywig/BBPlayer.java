5a890478ffc642d066a6fc9720b1d3f6b4773990
jkoppen-bluebillywig <j.koppen@bluebillywig.com>
2020-07-17 16:17:36 +0200

    added _bbdebug to component uri, removed bbdebug from mediaclip url

--- bbcomponent/src/main/java/com/bluebillywig/BBPlayer.java
+++ bbcomponent/src/main/java/com/bluebillywig/BBPlayer.java
@@ -288,9 +288,6 @@ public class BBPlayer extends WebView {
 		if (!setup.getShowCommercials()) {
 			mediaclipUrl += "?commercials=false";
 		}
-		if (setup.isDebug()) {
-			mediaclipUrl += ((mediaclipUrl.indexOf('?') > 0) ? "&" : "?") + "bbdebug=true";
-		}
 	}
 
 	/**
